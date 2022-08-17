51bdb48144b1b45d20fc6af7a80c634b8a350b3a
JoaoVictorMatulis <110988253+JoaoVictorMatulis@users.noreply.github.com>
2022-08-17 11:31:47 -0300

    Teste da primeira aula de Programação Web

---
+++ Teste.js
@@ -0,0 +1,30 @@
+function adicaoValores(valor1, valor2){
+    console.log("Iniciei a função 1")
+    let resultado = valor1 + valor2
+    return resultado;
+}
+console.log("Inicio do programa");
+let soma = adicaoValores(7, 6)
+console.log("A soma é: "+soma)
+
+/*
+Teste 1
+var nmClie
+function minhaPrimeiraFuncao(){
+    console.log("iniciei a função 1");
+    nmClie = "Joaquim.........";
+}
+console.log("Este é o primeiro STD do Programa");
+minhaPrimeiraFuncao();
+console.log("O cliente é: "+nmClie);
+
+Teste 2
+console.log("Bomdia");
+var nmCliente =  "Kleber";
+var idade = 20;
+if (idade < 18 || idade<20){
+    console.log(nmCliente+", você pode entrar");
+}
+else{
+    console.log(nmCliente+", você Não pode entrar");
+}*/
