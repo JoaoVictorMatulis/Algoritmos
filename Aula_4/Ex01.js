b5e5fd16ba9b69ba685c82f77aed9010a7b1615f
JoaoVictorMatulis <110988253+JoaoVictorMatulis@users.noreply.github.com>
2022-09-14 11:22:59 -0300

    Delete Aula_4 directory

--- Aula_4/Ex01.js
+++
@@ -1,9 +0,0 @@
-var arrayMarca = ['Ford','BMW','Fiat','Audi','Volkswagen'];
-function lista(){
-    let id = document.getElementById("listaMarca")
-    arrayMarca.forEach((item)=>{
-        var lista = document.createElement('li');
-        lista.innerText = item
-        id.appendChild(lista)
-    })
-}
